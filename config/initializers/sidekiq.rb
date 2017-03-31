c3cfd423f3c02f3160342ceb9dc721894975a613
borourke <borourke@crowdflower.com>
2017-03-31 10:59:47 -0700

    trying to fix connection pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,12 @@
+# three unicorns = 3 connections
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+# so one sidekiq can have 7 connections
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 7 }
+end
+
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file) && Sidekiq.server?
