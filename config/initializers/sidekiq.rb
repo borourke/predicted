07f55c31c067a4ab79b7a5b7dc644f4ece7a20c7
borourke <borourke@crowdflower.com>
2017-03-31 11:05:35 -0700

    more fiddling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,6 @@
-# three unicorns = 3 connections
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
-end
 # so one sidekiq can have 7 connections
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 7 }
+  config.redis = { :size => 27 }
 end
 
 schedule_file = "config/schedule.yml"
