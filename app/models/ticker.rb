38da715b9d1b97afb7e9b08664095a092c9b62b0
borourke <borourke@crowdflower.com>
2017-04-04 09:32:59 -0700

    fix last trade price logic

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -5,7 +5,7 @@ class Ticker < ApplicationRecord
     # The percent change will be positive if the price has gone up, or negative if it has gone down.
     #
     tickers = Ticker.where(contract_id: contract_id).order(ticker_at: :desc)
-    percent_change_10_min = (tickers.first.last_traded_price - tickers.second.last_traded_price) / tickers.second.last_traded_price * 100
+    percent_change_10_min = (tickers.first.last_traded_price || 0 - tickers.second.last_traded_price || 0) / tickers.second.last_traded_price || 0 * 100
     action = if percent_change_10_min.positive? && percent_change_10_min >= PERCENT_CHANGE_MIN_THRESHOLD
       :buy
     elsif percent_change_10_min.negative? && percent_change_10_min.abs >= PERCENT_CHANGE_MIN_THRESHOLD
